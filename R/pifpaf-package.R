1c8ce0eb0c9b472144b678bd5dfb0c57f59edf8d
Rodrigo Zepeda-Tello <RodrigoZepeda@users.noreply.github.com>
2023-09-02 01:36:50 -0400

    Closing #30
    Now `rr` and `cft` allow for a list.

--- R/pifpaf-package.R
+++ R/pifpaf-package.R
@@ -2,14 +2,28 @@
 "_PACKAGE"
 
 ## usethis namespace: start
+#' @importFrom cli cli
 #' @importFrom cli cli_abort
+#' @importFrom cli cli_alert_danger
+#' @importFrom cli cli_alert_info
 #' @importFrom cli cli_alert_warning
+#' @importFrom cli cli_end
+#' @importFrom cli cli_li
+#' @importFrom cli cli_rule
+#' @importFrom cli cli_ul
 #' @importFrom doParallel stopImplicitCluster
+#' @importFrom dplyr across
+#' @importFrom dplyr any_of
+#' @importFrom dplyr reframe
+#' @importFrom dplyr select
+#' @importFrom dplyr starts_with
+#' @importFrom dplyr summarise
 #' @importFrom foreach foreach
 #' @importFrom methods formalArgs
 #' @importFrom mvtnorm rmvnorm
 #' @importFrom parallel detectCores
 #' @importFrom Rdpack reprompt
+#' @importFrom S7 prop
 #' @importFrom stats qt
 #' @importFrom stats quantile
 #' @importFrom stats sd
@@ -18,5 +32,6 @@
 #' @importFrom svrep as_bootstrap_design
 #' @importFrom utils capture.output
 #' @importFrom utils citation
+#' @import mathjaxr
 ## usethis namespace: end
 NULL
